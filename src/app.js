3a44d8d9e5b7bf32c0d15c1b96471069dba58424
adarsha2003 <38953227+adarsha2003@users.noreply.github.com>
2021-07-17 02:13:47 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const db = require("./db/conn");
+const db = require("./db/database-config");
 const path = require("path");
 const app = express();
 const port = process.env.port || 3000;
