8c9783ff26d856c881514565989d4a8070186bd8
adarsha2003 <38953227+adarsha2003@users.noreply.github.com>
2021-07-21 11:15:18 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -4,10 +4,10 @@ const { Pool } = require("pg");
 // Configure the database connection.
 
 const config = {
-  user: "user",
-  password: "password",
-  host: "host",
-  database: "dbname",
+  user: "adarsha",
+  password: "TpRLzEIW3niIXKYO",
+  host: "free-tier8.aws-ap-southeast-1.cockroachlabs.cloud",
+  database: "passnotes-141.defaultdb",
   port: 26257,
   ssl: {
     rejectUnauthorized: false,
